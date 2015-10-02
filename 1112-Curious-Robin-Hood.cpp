818a218a1609f04d46aaf3cdfceb67dda568eeda
Vishnu Ks <hackerkid@vishnuks.com>
2015-10-02 23:38:22 +0530

    1112 Curious Robinhood added

---
+++ 1112-Curious-Robin-Hood.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#define SIZE 100005
+#include <string.h>
+#include <stdio.h>
+
+using namespace std;
+int a[SIZE];
+int inp[SIZE];
+int n;
+
+
+long long update(int i, int val)
+{
+
+	while(i < n) {
+		a[i] = a[i] + val;
+		i = i + (i & (-i));
+	}
+
+}
+
+long long query(int i)
+{	
+	long long ans;
+	ans = 0;
+
+	while(i > 0) {
+		ans = ans + a[i];
+		i = i - (i & (-i));
+	}
+
+	return ans;
+}
+
+int main()
+{
+	int t;
+	int x;
+	int y;
+	int w;
+	int choice;
+	int ans;
+	int q;
+	int ch;
+
+	scanf("%d", &t);
+
+	for (int cs = 1; cs <= t; cs++) {
+		scanf("%d", &n);
+		scanf("%d", &q);
+		
+		memset(a, 0, sizeof a);
+		memset(inp, 0, sizeof inp);
+
+		for (int i = 1; i <= n; i++) {
+			scanf("%d", &inp[i]);
+			update(i, inp[i]);
+		}
+
+		printf("Case %d:\n", cs);
+		for (int i = 0; i < q; i++) {
+			scanf("%d", &ch);
+			
+			switch(ch) {
+				case 1:
+					scanf("%d", &x);
+					x++;
+					update(x, -inp[x]);
+					printf("%d\n", inp[x]);
+					inp[x] = 0;
+					break;
+				case 2:
+					scanf("%d", &x);
+					x++;
+					scanf("%d", &w);
+					update(x, w);
+					inp[x] += w;
+					break;
+
+				case 3:
+					scanf("%d", &x);
+					scanf("%d", &y);
+					x++;
+					y++;
+					ans = query(y) - query(x -1);
+					printf("%d\n", ans);
+					break;
+
+			}
+		}
+	}
+}
+
+
+
+
+
