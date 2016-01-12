5edf0bd731eec01368a1226e2bae9775d3b81704
hackerkid <yo@vishnuks.com>
2016-01-13 02:07:12 +0530

    wrong solution, need fix

---
+++ 1076-Get-the-Container.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#define N 10005
+using namespace std;
+
+int main()
+{
+
+	int t;
+	int a[N];
+	long long bb;
+	long long sum[N];
+	int m;
+	long long maxi;
+	int n;
+	
+	scanf("%d", &t);
+
+	for (int cs = 1; cs <= t; cs++) {
+		scanf("%d", &n);
+		scanf("%d", &m);
+
+		sum[n ] = 0;
+		maxi = -1;
+		bb = 0;		
+		int j;
+		j = 0;
+
+		for (int i = 0; i < n; i++) {
+			scanf("%d", a + i);
+		}
+
+		for (int i = n - 1; i >= 0; i--) {
+			sum[i] = sum[i + 1] + a[i ];
+		}
+
+		for (int i = 0; i < n; i++) {
+		//	cout << sum[i] << " ";
+		}
+
+	//	cout << endl;
+
+		for (int i = 0; i < n; ) {
+			if(bb + a[i] <= maxi) {
+				bb = bb + a[i];
+				i++;
+			}
+			else {
+				if((double)(bb + a[i]) <= (double) sum[i] / (m - (j + 1))) {
+					bb = bb + a[i];
+					maxi = max(maxi, bb);
+					i++;
+				}
+				else {
+					j++;
+					bb = 0;
+				}
+
+			}
+		}
+
+
+		printf("Case %d: %d\n", cs, maxi);
+
+	}
+
+
+}
+
